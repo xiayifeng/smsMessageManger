49eb075b50adace6520960f38a9419b986a13d30
xiayifeng <441740874@qq.com>
2015-07-22 14:17:11 +0800

    create project

---
+++ src/com/xyf/SmsMessageManager/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.xyf.SmsMessageManager;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
